4dff9a6781000f6d7a937adfeda578ef504e9827
Greg Delgado III <grd888@gmail.com>
2016-11-29 20:09:40 +0800

    Add CountDownForm component and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,10 +4,10 @@ var Navigation = require('Navigation');
 var Main = (props) => {
   return (
     <div>
+      <Navigation />
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
 
-      <div>
-        <div>
-          <Navigation />
           <p>Main.jsx rendered</p>
           {props.children}
         </div>
